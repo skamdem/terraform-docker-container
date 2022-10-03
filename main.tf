7393953e77ffbec905082b47992335a9783c785c
sylvain kamdem <sylvain.kamdem@gmail.com>
2022-10-03 16:55:18 -0500

    rearranged code with the command 'terraform fmt' and added a README file

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 terraform {
-    required_providers {
-      docker = {
-          source    = "kreuzwerker/docker"
-          version   = "~> 2.22.0" #2.13.0
-      }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0" #2.13.0
     }
+  }
 }
 
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-    name            = "nginx:latest"
-    keep_locally    = false
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-    image           = docker_image.nginx.image_id
-    name            = "tutorial" 
-    ports {
-        internal     = 80
-        external    = 8000
-    }
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
