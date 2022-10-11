9ce90295eb6f77822400b01fb91a912536097619
sylvain kamdem <sylvain.kamdem@gmail.com>
2022-10-11 16:23:09 -0500

    refactored code to use varialbes

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = var.containerName
   ports {
     internal = 80
     external = 8000
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "container_name" {
+  description = "value of the container's name"
+  type        = string
+  default     = "tutorial"
+}
