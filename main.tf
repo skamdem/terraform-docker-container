9e5fccd1b8014bbcd37a44c5b6342f39b0332fea
Sylvain K <sylvain.kamdem@gmail.com>
2023-05-12 19:03:49 +0100

    splitted the files

--- main.tf
+++ main.tf
@@ -6,19 +6,3 @@ terraform {
     }
   }
 }
-
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = var.container_name
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
